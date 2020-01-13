6ef87f3bf07dea3a321d11def9e32f00522c40dd
Tim <github@timgreen.ws>
2020-01-13 16:35:07 +1100

    added User and Window Preferences

--- main.js
+++ main.js
@@ -9,3 +9,23 @@ const MainWindow = require('./app/main_window');
 const ElectronPrefs = require('electron-prefs');
 
 const { app, Menu, ipcMain, Notification } = electron;
+
+
+let mainWindow;
+let spinnerWindow;
+let offlineWindow;
+let tray;
+let isQuitting;
+let appIconPath;
+
+// User and Window Preferences
+const prefs = new ElectronPrefs({
+  fileName: 'config.js',
+  defaults: {
+    window: {
+      x: 0,
+      width: 800,
+      height: 600
+    }
+  }
+});
